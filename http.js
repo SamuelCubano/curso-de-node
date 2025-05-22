0e78bee2773a6de126d0e0ee359a8f00ef39420b
SamCub <samule10cubano@gmail.com>
2025-05-22 09:05:31 -0400

    2:32:12

--- http.js
+++ http.js
@@ -1,8 +1,25 @@
 const http = require('http')
 
-http.createServer(function (request, response) {
-    response.write('Hello world')
+http.createServer((request, response) => {
+
+    console.log(request.url)
+    
+    if (request.url === '/') {
+        response.write('Welcome to the server')
+        return response.end()
+    }
+    
+    if (request.url === '/about') {
+        response.write('acerca de')
+        return response.end()
+    }
+
+    response.write(`
+        <h1>NOT FOUND</h1>
+        <p>esta pagina no se encontró</p>
+        <a href="/">Volver al inicio</a>
+        `)
     response.end()
 }).listen(3000)
-
+ 
 console.log('servidor escuchando en el puerto 3000')
